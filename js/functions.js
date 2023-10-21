5b2ae385609de1e7d3280e0ca2495b1a5633030d
Elheid <Elfieleintsvard@mail.ru>
2023-10-21 19:09:13 +0500

    module5-task2 5.16 сделал функцию

--- js/functions.js
+++ js/functions.js
@@ -25,7 +25,48 @@ const getDigits = (str) => {
   }
   return result === '' ? NaN: result;
 };
-getDigits();
-checkPalindrom();
-getStrLength();
+getDigits('123');
+checkPalindrom('12321');
+getStrLength('23', 2);
+//Задание 5.16
+const summTime = (hours, minutes) => {
+  const firstNum = hours.split('.');
+  let resHours = Number(firstNum[0]) + Math.floor(minutes / 60);
+  let resMinutes = Number(firstNum[1]) + minutes % 60;
+  if (resMinutes > 60) {
+    resHours += Math.floor(resMinutes/60);
+    resMinutes += resMinutes % 60;
+  }
+  return parseFloat(`${resHours}.${resMinutes}`);
+};
+const correctMinutes = (times)=>{
+  for (let i = 0; i < times.length; i++){
+    times[i] = times[i].split(':');
+  }
+  for (let i = 0; i < times.length; i++){
+    let secondPart = times[i][1];
+    const firstPart = times[i][0];
+    if (secondPart.length === 1){
+      secondPart = `0${secondPart}`;
+    }
+    times[i] = `${firstPart}.${secondPart}`;
+  }
+  return times;
+};
+const isMeetingInWorkHours = (startTime, endTime, meetingStart, duration) => {
+  const times = [startTime, endTime, meetingStart];
+  const startTimeIndex = 0;
+  const endTimeIndex = 1;
+  const meetingTimeIndex = 2;
+  correctMinutes(times);
+  if (parseFloat(times[meetingTimeIndex]) < parseFloat(times[startTimeIndex])){
+    return false;
+  }
+  if (summTime(times[meetingTimeIndex], duration) > times[endTimeIndex]){
+    return false;
+  }
+  return true;
+};
 
+isMeetingInWorkHours('08:00', '17:30', '14:00', 90); // true
+isMeetingInWorkHours('8:5', '10:0', '8:50', 90);     // false
