e4a17f80aab5e1b9ff4386a190b9993043de735b
Elheid <Elfieleintsvard@mail.ru>
2023-11-22 17:47:25 +0500

    8.15. Открывается и закрывается (часть 2)

--- js/bigPicture.js
+++ js/bigPicture.js
@@ -1,45 +1,60 @@
 import { renderComments } from './render.js';
-
 const bigPicture = document.querySelector('.big-picture');
 const commentsContainer = bigPicture.querySelector('.social__comments');
 const countOfComments = bigPicture.querySelector('.social__comment-count');
-const commentsLoader = bigPicture.querySelector('.comments-loader');
+const commentsLoaderButton = bigPicture.querySelector('.comments-loader');
 const cancelButton = bigPicture.querySelector('.big-picture__cancel');
+const counter = countOfComments.textContent;
+let currCount = Number(counter.slice(0,1));
+const maxCount = countOfComments.querySelector('span');
+const maxCountTextContent = maxCount.textContent;
 
 const hideBigPicture = ()=>{
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', closeOnKey);
 };
 
-const closeOnKey = (evt)=> {
-  if (evt.key === 'Escape'){
-    evt.preventDefault();
-    hideBigPicture();
-  }
-};
+function closeOnKey (evt) {
+  if (evt.key === 'Escape'){ //в hideBigPicture должен быть closeOnKey
+    evt.preventDefault();   // и в closeOnKey должен быть hideBigPicture
+    hideBigPicture();       // что-то нужно объявлять раньше другого, а оставшуюся объявлять через function
+  }                        // чтобы была возможность использовать после объявления
+}
 
 const closeButtonOnClick = ()=>{
   hideBigPicture();
 };
+cancelButton.addEventListener('click', closeButtonOnClick);
 
-const getPictureDetails = ({url, likes, description})=> {
+const getPictureDetails = ({url, likes, description, comments})=> {
   const image = bigPicture.querySelector('.big-picture__img img');
   image.src = url;
   image.alt = description;
   bigPicture.querySelector('.likes-count').textContent = likes;
   bigPicture.querySelector('.social__caption').textContent = description;
+  maxCount.textContent = comments.length;
 };
 
-cancelButton.addEventListener('click', closeButtonOnClick);
+const reloadCounters = ()=> {
+  countOfComments.textContent = `${currCount} из ${maxCountTextContent}`;
+};
 
 const showBigPicture = (miniature) => {
   bigPicture.classList.remove('hidden');
+  commentsLoaderButton.addEventListener('click', ()=>
+  {
+    if (currCount !== Number(maxCountTextContent)){
+      currCount += 5;
+    }
+    reloadCounters();
+    renderComments(miniature.comments, currCount);
+
+  });
   document.body.classList.add('modal-open');
-  commentsLoader.classList.add('hidden');
-  countOfComments.classList.add('hidden');
   document.addEventListener('keydown', closeOnKey);
   getPictureDetails(miniature);
-  renderComments(miniature.comments, commentsContainer);
+  renderComments(miniature.comments, currCount);
 };
 
-export {showBigPicture};
+export {showBigPicture, commentsContainer};
--- js/render.js
+++ js/render.js
@@ -1,24 +1,8 @@
-import { showBigPicture } from './bigPicture.js';
+import { showBigPicture, commentsContainer } from './bigPicture.js';
+import { createMiniatures, createComment } from './objectCreator.js';
 
-const miniaturesTemplate = document.querySelector('#picture')
-  .content
-  .querySelector('.picture');
 const picturesList = document.querySelector('.pictures');
 const picturesFragment = document.createDocumentFragment();
-
-const createMiniatures = ({url, likes, description, comments}) => {
-  const miniatureElement = miniaturesTemplate.cloneNode(true);
-  const miniatureComments = miniatureElement.querySelector('.picture__comments');
-  const miniatureLikes = miniatureElement.querySelector('.picture__likes');
-  const picture = miniatureElement.querySelector('.picture__img');
-  picture.src = url;
-  picture.alt = description;
-  miniatureComments.textContent = comments.length;
-  miniatureLikes.textContent = likes;
-
-  return miniatureElement;
-};
-
 
 const renderMiniatures = (pictures) => {
   pictures.forEach((picture) => {
@@ -32,40 +16,13 @@ const renderMiniatures = (pictures) => {
   picturesList.append(picturesFragment);
 };
 
-const createCommentPattern = ()=> {
-  const newComment = document.createElement('li');
-  newComment.classList.add('social__comment');
-  const image = document.createElement('img');
-  image.classList.add('social__picture');
-  image.src = '';
-  image.alt = '';
-  image.width = 35;
-  image.height = 35;
-
-  const text = document.createElement('p');
-  text.classList.add('social__text');
-  newComment.append(image);
-  newComment.append(text);
-  return newComment;
-};
-
-const createComment = ({avatar, name, message})=>{
-  const comment = createCommentPattern();
-  comment.querySelector('.social__picture').src = avatar;
-  comment.querySelector('.social__picture').alt = name;
-  comment.querySelector('.social__text').textContent = message;
-  return comment;
-};
-
-const renderComments = (listComments, commentsContainer) => {
-  commentsContainer.innerHTML = '';
+const renderComments = (listComments, loadCount) => {
   const fragment = document.createDocumentFragment();
-  listComments.forEach((item) => {
-    const comment = createComment(item);
-    fragment.append(comment);
-  });
+  commentsContainer.innerHTML = '';
+  for (let i = 0; i < loadCount; i++){
+    fragment.append(createComment(listComments[i]));
+  }
   commentsContainer.append(fragment);
 };
-
 
 export {renderMiniatures, renderComments};
